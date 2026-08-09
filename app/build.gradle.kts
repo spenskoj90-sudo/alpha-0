64b6ace03113c7c42a7a3ae78382b17d40065fc1
spenskoj90-sudo <spenskoj90@gmail.com>
2026-08-09 18:46:48 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,13 +12,39 @@ android {
         minSdk = 29
         targetSdk = 35
 
-        versionCode = 1
-        versionName = "0.1.0"
+        versionCode = 2
+        versionName = "0.2.0"
+    }
+
+    signingConfigs {
+        create("ciRelease") {
+            val keystorePath = System.getenv("ALPHA0_KEYSTORE_PATH")
+            val keystorePassword = System.getenv("ALPHA0_KEYSTORE_PASSWORD")
+            val keyAlias = System.getenv("ALPHA0_KEY_ALIAS")
+            val keyPassword = System.getenv("ALPHA0_KEY_PASSWORD")
+
+            if (
+                keystorePath != null &&
+                keystorePassword != null &&
+                keyAlias != null &&
+                keyPassword != null
+            ) {
+                storeFile = file(keystorePath)
+                storePassword = keystorePassword
+                this.keyAlias = keyAlias
+                this.keyPassword = keyPassword
+            }
+        }
     }
 
     buildTypes {
+        debug {
+            signingConfig = signingConfigs.getByName("ciRelease")
+        }
+
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("ciRelease")
         }
     }
 }

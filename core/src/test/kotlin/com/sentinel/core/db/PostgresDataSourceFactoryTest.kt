9b908f20bb2096b985112a156e3a672071530129
spenskoj90-sudo <spenskoj90@gmail.com>
2026-08-10 16:07:25 +0300

    Test PostgreSQL security parameter enforcement

--- core/src/test/kotlin/com/sentinel/core/db/PostgresDataSourceFactoryTest.kt
+++ core/src/test/kotlin/com/sentinel/core/db/PostgresDataSourceFactoryTest.kt
@@ -1,6 +1,5 @@
 package com.sentinel.core.db
 
-import org.junit.Assert.assertEquals
 import org.junit.Assert.assertThrows
 import org.junit.Test
 
@@ -18,6 +17,19 @@ class PostgresDataSourceFactoryTest {
             PostgresDataSourceFactory.fromEnvironment(
                 mapOf(
                     "SENTINEL_DB_URL" to "jdbc:h2:mem:test",
+                    "SENTINEL_DB_USER" to "sentinel",
+                    "SENTINEL_DB_PASSWORD" to "secret"
+                )
+            )
+        }
+    }
+
+    @Test
+    fun callerCannotOverrideTlsOrChannelBinding() {
+        assertThrows(IllegalArgumentException::class.java) {
+            PostgresDataSourceFactory.fromEnvironment(
+                mapOf(
+                    "SENTINEL_DB_URL" to "jdbc:postgresql://db/sentinel?sslmode=disable",
                     "SENTINEL_DB_USER" to "sentinel",
                     "SENTINEL_DB_PASSWORD" to "secret"
                 )

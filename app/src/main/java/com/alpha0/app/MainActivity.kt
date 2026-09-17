22d52fe831a6e06c0962e9f26b6a9afb6109b4c0
spenskoj90-sudo <spenskoj90@gmail.com>
2026-09-17 07:55:50 +0300

    android: fix scoped weight resolution

--- app/src/main/java/com/alpha0/app/MainActivity.kt
+++ app/src/main/java/com/alpha0/app/MainActivity.kt
@@ -14,7 +14,6 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text

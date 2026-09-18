b41984e13fb1606025f88f4d74bb10c0c9a76b25
spenskoj90-sudo <spenskoj90@gmail.com>
2026-09-18 21:17:17 +0300

    build(android): migrate to AGP 9.3 and Kotlin 2.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
+buildscript {
+    dependencies {
+        // AGP 9 built-in Kotlin defaults to KGP 2.2.10. SENTINEL pins the
+        // highest KGP version fully supported by the selected AGP/Gradle pair.
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.4.20")
+    }
+}
+
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("org.jetbrains.kotlin.android") version "2.0.21" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    id("com.android.application") version "9.3.1" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
 }

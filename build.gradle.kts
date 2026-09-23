63c6eff16fa8e866c3bf086dbc8a9d1ebd992de5
spenskoj90-sudo <spenskoj90@gmail.com>
2026-09-23 08:03:19 +0300

    chore: modernize build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,6 @@ buildscript {
 }
 
 plugins {
-    id("com.android.application") version "9.3.1" apply false
+    id("com.android.application") version "9.4.0" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.4.20" apply false
 }

ca978aecf7d419968f0e2ebe0f101091c91ede65
spenskoj90-sudo <spenskoj90@gmail.com>
2026-09-25 09:12:51 +0300

    chore(android): move compose instrumentation to v2 rule

--- app/src/androidTest/java/com/alpha0/app/ui/AccessibilitySemanticsTest.kt
+++ app/src/androidTest/java/com/alpha0/app/ui/AccessibilitySemanticsTest.kt
@@ -8,7 +8,7 @@ import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.semantics.SemanticsProperties
 import androidx.compose.ui.test.SemanticsMatcher
 import androidx.compose.ui.test.assert
-import androidx.compose.ui.test.junit4.createComposeRule
+import androidx.compose.ui.test.junit4.v2.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
 import org.junit.Rule
 import org.junit.Test

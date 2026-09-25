2a155a98a97ee92a79f29b503e88c43c14934fc6
spenskoj90-sudo <spenskoj90@gmail.com>
2026-09-25 18:21:40 +0300

    chore(android): update Navigation Compose stable patch

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -229,7 +229,7 @@ dependencies {
     implementation("androidx.core:core-ktx:1.19.1")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.11.0")
     implementation("androidx.activity:activity-compose:1.13.0")
-    implementation("androidx.navigation:navigation-compose:2.10.1")
+    implementation("androidx.navigation:navigation-compose:2.10.2")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.ui:ui-text-google-fonts")
